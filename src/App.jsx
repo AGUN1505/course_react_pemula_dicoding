c6fa8b5f75bf6bf848538f5561f301a7e75cfcde
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2026-01-19 21:59:18 +0700

    adding app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+
+const nama = 'Agun';
+ 
+const element = (
+  <div>
+    <h1>Biodata Perusahaan</h1>
+    <ul>
+      <li>Nama: Dicoding Indonesia {nama}</li>
+      <li>Bidang: Education</li>
+      <li>Tagline: Decode Ideas, Discover Potential.</li>
+      <img src="dicoding-logo.png" alt="Dicoding Logo" />
+    </ul>
+  </div>
+);
+
+export default element;
+ 
+// // console.log(element);
+// const root = createRoot(document.getElementById('root'));
+// root.render(element);
